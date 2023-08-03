95356839979b116b4b0afad31b91367c6078f2cd
Mansheel Randhawa <mansheelrandhawa2001@gmail.com>
2023-08-03 21:04:12 +0800

    implemented Exchangeable interface

--- Exchangeable.java
+++ Exchangeable.java
@@ -1,3 +1,7 @@
-public class Exchangeable {
+public interface Exchangeable {
+  final double ED_TO_MM = 1.3;
+  final double ED_TO_SS = 0.87;
+  final double ED_TO_NN = 2;
   
+  public void exchange(Currency other, double amount);
 }
