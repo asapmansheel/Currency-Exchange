a77eb4c182b4e1c5a02608e9590eeb1404446c7e
Mansheel Randhawa <mansheelrandhawa2001@gmail.com>
2023-08-03 20:15:58 +0800

    added getters and setters in Currency class

---
+++ Currency.java
@@ -0,0 +1,24 @@
+public class Currency {
+  String currencyName;
+  String planetName;
+  double totalFunds;
+
+  public String getCurrencyName() {
+    return currencyName;
+  }
+  public void setCurrencyName(String currencyName) {
+    this.currencyName = currencyName;
+  }
+  public String getPlanetName() {
+    return planetName;
+  }
+  public void setPlanetName(String planetName) {
+    this.planetName = planetName;
+  }
+  public double getTotalFunds() {
+    return totalFunds;
+  }
+  public void setTotalFunds(double totalFunds) {
+    this.totalFunds = totalFunds;
+  }
+}
